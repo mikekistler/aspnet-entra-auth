58cbbd7b3d1b8254b4516dd68421f3292592f667
Mike Kistler <mikekistler@microsoft.com>
2025-03-09 16:24:10 -0500

    Delete docref tags

--- Program.cs
+++ Program.cs
@@ -1,11 +1,8 @@
-// <ms_docref_import_types>
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
-// </ms_docref_import_types>
 
-// <ms_docref_add_msal>
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 IEnumerable<string>? initialScopes = builder.Configuration.GetSection("DownstreamApi:Scopes").Get<IEnumerable<string>>();
 
@@ -13,9 +10,7 @@ builder.Services.AddMicrosoftIdentityWebAppAuthentication(builder.Configuration,
     .EnableTokenAcquisitionToCallDownstreamApi(initialScopes)
         .AddDownstreamApi("DownstreamApi", builder.Configuration.GetSection("DownstreamApi"))
         .AddInMemoryTokenCaches();
-// </ms_docref_add_msal>
 
-// <ms_docref_add_default_controller_for_sign-in-out>
 builder.Services.AddRazorPages().AddMvcOptions(options =>
     {
         var policy = new AuthorizationPolicyBuilder()
@@ -23,14 +18,11 @@ builder.Services.AddRazorPages().AddMvcOptions(options =>
                       .Build();
         options.Filters.Add(new AuthorizeFilter(policy));
     }).AddMicrosoftIdentityUI();
-// </ms_docref_add_default_controller_for_sign-in-out>
 
-// <ms_docref_enable_authz_capabilities>
 WebApplication app = builder.Build();
 
 app.UseAuthentication();
 app.UseAuthorization();
-// </ms_docref_enable_authz_capabilities>
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
